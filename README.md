9989466afa600b5d831ff4fc3bd74bc2dfa274a3
Emerson Rocha Luiz <emerson@alligo.com.br>
2020-03-29 21:56:49 -0300

    Adicionado arquivos para poder pre-visualizar GitHub pages localmente

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# dados-v1
-[rascunho]
+# covid.etica.ai/dados-v1
+
+## Fontes de dados, por conteúdo
+
+### Situação de casos confirmados, suspeitos, recuperados e óbitos
+
+### TODO...
---
+++ ajuda.sh
@@ -0,0 +1,10 @@
+# Nao execute esse script por acidente
+exit 1
+
+### Pre-visualizar resultado gerado pelo GitHub Pages
+## Depurar localmente
+# Isso instala as dependencias do Jekyll
+bundle install
+
+# Roda o Jekyll localmente em http://localhost:4000
+bundle exec jekyll serve
